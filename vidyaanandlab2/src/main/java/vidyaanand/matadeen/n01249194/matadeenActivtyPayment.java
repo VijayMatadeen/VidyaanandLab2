e3052becffa4607aa0e104abf97f0b366632a9b5
VijayMatadeen <71228563+VijayMatadeen@users.noreply.github.com>
2020-11-10 18:44:52 -0500

    add empty Activities check out, real state info, and payment

---
+++ vidyaanandlab2/src/main/java/vidyaanand/matadeen/n01249194/matadeenActivtyPayment.java
@@ -0,0 +1,14 @@
+package vidyaanand.matadeen.n01249194;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class matadeenActivtyPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matadeen_activty_payment);
+    }
+}
