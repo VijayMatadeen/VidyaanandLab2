d0ad5d7919a59a029384566a784ee153fa7b7978
VijayMatadeen <71228563+VijayMatadeen@users.noreply.github.com>
2020-11-10 17:52:58 -0500

    Initial commit

---
+++ app/src/main/java/vidyaanand/matadeen/n01249194/MainActivity.java
@@ -0,0 +1,14 @@
+package vidyaanand.matadeen.n01249194;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
